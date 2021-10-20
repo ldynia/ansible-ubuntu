bed66f044527ee884947d3d04a6f355552e8e458
ldynia <l_dynowski@yahoo.com>
2021-10-20 19:35:05 +0200

    Update install.sh

--- roles/modern_unix_commands/install.sh
+++ roles/modern_unix_commands/install.sh
@@ -7,19 +7,19 @@ if [ $(uname -m) == 'x86_64' ]; then
   ARCHITECTURE='amd64'
 fi
 
-# # Install LSD
-# cd /tmp
-# wget "https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd_0.20.1_${ARCHITECTURE}.deb"
-# sudo dpkg -i lsd_0.20.1_${ARCHITECTURE}.deb
-# rm lsd_0.20.1_${ARCHITECTURE}.deb
+# Install LSD
+cd /tmp
+wget "https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd_0.20.1_${ARCHITECTURE}.deb"
+sudo dpkg -i lsd_0.20.1_${ARCHITECTURE}.deb
+rm lsd_0.20.1_${ARCHITECTURE}.deb
 
-# # Install fonts for LSD
-# mkdir -p ~/.local/share/fonts
-# cd ~/.local/share/fonts
-# wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/3270.zip"
-# unzip -o 3270.zip
-# rm 3270.zip
-# find  -type f -name "*Windows*" -delete
+# Install fonts for LSD
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts
+wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/3270.zip"
+unzip -o 3270.zip
+rm 3270.zip
+find  -type f -name "*Windows*" -delete
 
 # Install EXA
 mkdir -p /tmp/exa
