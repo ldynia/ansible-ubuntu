8798f402a22580b6593a00df53f6fc730bc8150d
ldynia <l_dynowski@yahoo.com>
2021-10-20 18:09:16 +0200

    Create install.sh

---
+++ roles/modern_unix_commands/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# MODERN UNIX COMMANDS
+# This script is installation of commands listed in https://github.com/ibraheemdev/modern-unix
+
+ARCHITECTURE=$(uname -m)
+if [ $(uname -m) == 'x86_64' ]; then
+  ARCHITECTURE='amd64'
+fi
+
+# Install LSD
+cd /tmp
+wget "https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd_0.20.1_${ARCHITECTURE}.deb"
+sudo dpkg -i lsd_0.20.1_${ARCHITECTURE}.deb
+rm lsd_0.20.1_${ARCHITECTURE}.deb
+
+# Install fonts for LSD
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts
+wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/3270.zip"
+unzip -o 3270.zip
+rm 3270.zip
+find  -type f -name "*Windows*" -delete
